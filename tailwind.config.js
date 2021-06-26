d867a79fc7a93a2cbdf901e95356ae400598260e
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-06-26 22:19:43 +0200

    whitelists some shadows from the purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.elm"],
-    safelist: ["p-10", "p-6", "p-7", "p-8"]
+    safelist: ["p-10", "p-6", "p-7", "p-8", "shadow-2xl", "shadow-lg"]
   },
   darkMode: "media",
   theme: {
