2ba56ce3c7185c1a1a36ef6759cd191ef9509708
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-06-27 13:50:21 +0200

    adding a static header to the views and possible pages to data model

--- src/Main.elm
+++ src/Main.elm
@@ -1,23 +1,30 @@
 module Main exposing (main)
 
 import Browser exposing (Document)
-import Html exposing (Html, a, footer, h1, li, nav, text, ul)
-import Html.Attributes exposing (classList, href)
+import Html exposing (Html, a, div, footer, h1, h2, li, nav, text, ul)
+import Html.Attributes exposing (class, classList, href)
+
+
+
+-- MODEL
 
 
 type alias Model =
-    {}
+    { page : Page }
+
+
+type Page
+    = Week
+    | Day
+    | NotFound
+
+
+
+-- UPDATE
 
 
 type Msg
     = NothingYet
-
-
-view : Model -> Document Msg
-view model =
-    { title = "Pollen levels in Hamburg"
-    , body = [ text "Mutation in progress." ]
-    }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -30,11 +37,38 @@ subscriptions model =
     Sub.none
 
 
+
+-- MAIN
+
+
 main : Program () Model Msg
 main =
     Browser.document
-        { init = \_ -> ( {}, Cmd.none )
+        { init = \_ -> ( { page = Week }, Cmd.none )
         , subscriptions = subscriptions
         , update = update
         , view = view
         }
+
+
+
+-- VIEW
+
+
+view : Model -> Document Msg
+view model =
+    { title = "Pollen levels in Hamburg"
+    , body = [ headerView ]
+    }
+
+
+headerView : Html Msg
+headerView =
+    div [ class "bg-gradient-to-b from-gray-500 via-gray-700 to-gray-500 bg-gray-500 min-h-screen" ]
+        [ div [ class "container p-4 md:p-6 mx-auto max-w-2xl text-white text-center" ]
+            [ h1 [ class "text-3xl font-black tracking-tight pt-10 filter drop-shadow-lg" ]
+                [ text "Pollen Levels" ]
+            , h2 [ class "text-3xl font-thin tracking-wider pb-14 filter drop-shadow-lg uppercase" ]
+                [ text "in Hamburg" ]
+            ]
+        ]
