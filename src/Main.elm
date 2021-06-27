cc95106162e1d1625f978bf3caac17c8d359c22e
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-06-27 15:21:42 +0200

    renders (not yet functional) back button

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,9 @@ module Main exposing (main)
 import Browser exposing (Document)
 import Html exposing (Html, a, div, footer, h1, h2, li, nav, text, ul)
 import Html.Attributes exposing (class, classList, href)
+import Html.Lazy exposing (lazy)
+import Svg
+import Svg.Attributes
 
 
 
@@ -58,17 +61,48 @@ main =
 view : Model -> Document Msg
 view model =
     { title = "Pollen levels in Hamburg"
-    , body = [ headerView ]
+    , body = [ lazy headerView model.page ]
     }
 
 
-headerView : Html Msg
-headerView =
+headerView : Page -> Html msg
+headerView page =
+    let
+        back =
+            case page of
+                Week ->
+                    div [] []
+
+                _ ->
+                    backIconView
+    in
     div [ class "bg-gradient-to-b from-gray-500 via-gray-700 to-gray-500 bg-gray-500 min-h-screen" ]
         [ div [ class "container p-4 md:p-6 mx-auto max-w-2xl text-white text-center" ]
-            [ h1 [ class "text-3xl font-black tracking-tight pt-10 filter drop-shadow-lg" ]
-                [ text "Pollen Levels" ]
-            , h2 [ class "text-3xl font-thin tracking-wider pb-14 filter drop-shadow-lg uppercase" ]
-                [ text "in Hamburg" ]
+            [ div [ class "grid grid-cols-12 pb-14 place-items-center pt-10" ]
+                [ div [ class "col-span-2 pl-18 sm:pl-40" ] [ back ]
+                , div [ class "col-span-8" ]
+                    [ h1 [ class "text-3xl font-black tracking-tight filter drop-shadow-lg" ]
+                        [ text "Pollen Levels" ]
+                    , h2 [ class "text-3xl font-thin tracking-wider filter drop-shadow-lg uppercase" ]
+                        [ text "in Hamburg" ]
+                    ]
+                ]
             ]
+        ]
+
+
+backIconView : Svg.Svg msg
+backIconView =
+    Svg.svg
+        [ Svg.Attributes.width "42"
+        , Svg.Attributes.height "42"
+        , Svg.Attributes.viewBox "0 0 20 20"
+        , Svg.Attributes.fill "currentColor"
+        ]
+        [ Svg.path
+            [ Svg.Attributes.fillRule "evenodd"
+            , Svg.Attributes.stroke "currentColor"
+            , Svg.Attributes.d "M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z"
+            ]
+            []
         ]
